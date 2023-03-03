6afbed07c8502b509cde005ffcded28475c59324
marshall86 <antonio11061986@gmail.com>
2023-03-03 11:41:32 +0100

    dev tools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { isDevMode, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { StoreModule } from "@ngrx/store";
@@ -9,6 +9,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 // import { postReducer } from "./store/post/post.reducer";
 // import { messageReducer } from "./store/message/message.reducer";
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -22,7 +24,14 @@ import { AppComponent } from "./app.component";
     })*/
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot()
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ]
 })
 export class AppModule { }
